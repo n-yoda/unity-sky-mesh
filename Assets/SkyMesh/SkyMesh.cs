09653f58ea987683fae990db0aff84bcc4d046bd
Nobuki Yoda <nobuki.y@gmail.com>
2015-10-18 02:09:51 +0900

    First commit.

---
+++ Assets/SkyMesh/SkyMesh.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Draw a user-defined mesh as sky.
+/// The shader of the material should be SkyMesh shader.
+/// Skybox is recommended if you use GI.
+/// </summary>
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class SkyMesh : MonoBehaviour
+{
+    [SerializeField] Mesh m_mesh;
+    [SerializeField] Material m_material;
+    [SerializeField] CameraEvent m_cameraEvent = CameraEvent.AfterImageEffectsOpaque;
+    [SerializeField] Vector3 m_position = Vector3.zero;
+    [SerializeField] Vector3 m_rotation = Vector3.zero;
+    [SerializeField] Vector3 m_scale = Vector3.one;
+
+    CameraEvent lastCameraEvent;
+    CommandBuffer commandBuffer;
+
+    public Mesh mesh
+    {
+        get { return m_mesh; }
+        set
+        {
+            m_mesh = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+    public Material material {
+        get { return m_material; }
+        set
+        {
+            m_material = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+    public CameraEvent cameraEvent {
+        get { return m_cameraEvent; }
+        set
+        {
+            m_cameraEvent = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+    public Vector3 position {
+        get { return m_position; }
+        set
+        {
+            m_position = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+    public Vector3 rotation {
+        get { return m_rotation; }
+        set
+        {
+            m_rotation = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+    public Vector3 scale {
+        get { return m_scale; }
+        set
+        {
+            m_scale = value;
+            if (enabled) OnEnable();
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        if (enabled)
+        {
+            if (GetComponent<Camera>().clearFlags == CameraClearFlags.Skybox)
+                Debug.LogWarning("SkyMesh: You should turn off Skybox.");
+
+            OnEnable();
+        }
+    }
+#endif
+
+    void OnEnable()
+    {
+        OnDisable();
+        var camera = GetComponent<Camera>();
+        commandBuffer = new CommandBuffer();
+        commandBuffer.name = "SkyMesh";
+        commandBuffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+        var matrix = Matrix4x4.TRS(m_position, Quaternion.Euler(m_rotation), m_scale);
+        commandBuffer.DrawMesh(m_mesh, matrix, m_material);
+        camera.AddCommandBuffer(m_cameraEvent, commandBuffer);
+        lastCameraEvent = m_cameraEvent;
+    }
+
+    void OnDisable()
+    {
+        if (commandBuffer == null)
+            return;
+
+        var camera = GetComponent<Camera>();
+        camera.RemoveCommandBuffer(lastCameraEvent, commandBuffer);
+        commandBuffer.Dispose();
+        commandBuffer = null;
+    }
+}
---
+++ Assets/SkyMesh/SkyMesh.shader
@@ -0,0 +1,57 @@
+﻿Shader "Custom/SkyMesh"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		[Enum(CullMode)] _CullMode ("Cull Mode", float) = 1
+        [Toggle] _Finite ("Finite Radius", float) = 0
+	}
+	SubShader
+	{
+		Tags { "Queue"="Geometry+500" }
+		Cull [_CullMode]
+		ZWrite Off
+		Pass
+		{
+			CGPROGRAM
+            #pragma multi_compile _FINITE_OFF _FINITE_ON
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+                #if _FINITE_ON
+                    o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+                #else
+                    o.vertex = mul(UNITY_MATRIX_MVP, float4(v.vertex.xyz, 0));
+                #endif
+				o.vertex.z = o.vertex.w;
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return tex2D(_MainTex, i.uv);
+			}
+			ENDCG
+		}
+	}
+}
